15b934fe530996ca6239e14815bfca54cb8f98cf
Chelsea Dole <chelsea.dole@zonarsystems.com>
2018-06-14 10:27:50 -0700

    Adding flask-restplus to backend. Swagger ui.

--- run.py
+++ run.py
@@ -1,4 +1,32 @@
-from bpmninja import app
+"""Init script."""
+from flask import Flask, Blueprint
+from flask_restplus import Api
+
+from bpmninja.bpm import bpm_ns
+
+app = Flask(__name__)
+app.config['RESTPLUS_VALIDATE'] = True
+
+
+api = Api(app,
+          version='1.0',
+          title='BPM Ninja API',
+          description='A simple flask API to handle the backend of bpmninja'
+          )
+
+
+def initialize_app():
+    """Initialize bpmninja API flask app. Register ns (namespaces) and blueprints."""
+
+    # Register all API namespaces:
+    api.add_namespace(bpm_ns, path='/bpm')
+
+    bp = Blueprint('api', __name__, url_prefix='/api/v1')
+    api.init_app(bp)
+
+    app.register_blueprint(bp)
+
 
 if __name__ == '__main__':
+    initialize_app()
     app.run(host='127.0.0.1', port=5000, debug=True)
