ba7687c60a680416a9dba0adfabf699845c239e2
Chelsea Dole <chelsea.dole@zonarsystems.com>
2018-06-14 13:10:10 -0700

    ADding db conn layout

--- bpmninja/db_connection.py
+++ bpmninja/db_connection.py
@@ -1,5 +1,6 @@
 """DB connection spec for bpms, and users."""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime, func, Integer
 from run import app
 
 db = SQLAlchemy(app)
@@ -10,6 +11,14 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
+    allScores = db.Table('scores',
+                         db.Column('date', DateTime, default=func.now()),
+                         db.Column('score', Integer),
+                         db.Column('high', Integer),
+                         db.Column('low', Integer)
+                         )
+    avgScore = db.Column(db.Integer)
+    highScore = db.Column(db.Integer)
 
     def __repr__(self):
         return '<User %r>' % self.username
