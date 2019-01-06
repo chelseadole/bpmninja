1342a7d832fc442add890b020fd5acaeb55aedc7
CHELSEA DOLE <chelseadole@gmail.com>
2019-01-05 16:58:53 -0800

    deleting sqlalchemy stuff. added song_metadata.

--- bpmninja/bpm.py
+++ bpmninja/bpm.py
@@ -1,7 +1,6 @@
 """API routing for /GET endpoints, to get BPM data by song ID."""
+import song_metadata
 from flask_restplus import Resource, Namespace, reqparse
-
-from bpmninja.db import Session, Song
 
 bpm_ns = Namespace("bpm", description="GET bpm data by song ID")
 
@@ -23,46 +22,11 @@ class BPM(Resource):
     def get(self):
         """Get all bpm data."""
         args = bpm_parser.parse_args()
-        session = Session()
 
         if args['songId']:
             try:
-                result = session.query(Song).filter_by(id=args['songId']).all()
-                print('RESULT', result)
+                result = song_metadata[args['songId']]
                 return result, 200
-            except ValueError as err:
-                print('DB Query Error:', err.args)
-            # single_song = {
-            #     123: {
-            #         "bpm": 155,
-            #         "songTitle": "A Smooth One",
-            #         "artist": "Charlie Christian",
-            #         "album": "Best of Charlie Christian",
-            #         "artwork": "format tbd"
-            #     }
-            # }
-
-        else:
-            multi_song = [
-                {
-                    12: {
-                        "bpm": 155,
-                        "songTitle": "A Smooth One",
-                        "artist": "Charlie Christian",
-                        "album": "Best of Charlie Christian",
-                        "artwork": "format tbd"
-                    }
-                },
-                {
-                    13: {
-                        "bpm": 199,
-                        "songTitle": "Flying Home",
-                        "artist": "Lionel Hampton",
-                        "album": "Lionel's Hits",
-                        "artwork": "format tbd"
-                    }
-                }
-
-            ]
-
-            return multi_song, 200
+            except KeyError as err:
+                return 'songId {} is invalid'.format(err), 404
+        return 'songId is required', 404
